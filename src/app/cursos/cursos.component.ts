4550ebac663340d4351ce75ee313c4b634d51eeb
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2023-10-27 20:58:25 -0300

    Introdução aos Templates

---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit{
+
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'Ext Js', 'Angular'];
+
+  constructor(){
+    this.nomePortal  = 'http://loiane.training';
+
+    for (let i=0; i<this.cursos.length; i++){
+      let curso = this.cursos[i];
+    }
+  }
+
+  ngOnInit(){
+  }
+
+}
